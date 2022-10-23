b6616a0fd2edd463a7e8b137d9d0f886d486ea7c
fatima <fb71459@gmail.com>
2022-10-23 23:28:36 +0500

    Added a component , passing array and printing it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+//component added
+const Hello = ({arr}) => {
+    return (
+        <div>
+            {
+                arr.map(title => {
+                    return <div key={title}>{title}</div>    //printing the array using map function
+                })
+            }
+        </div>
+    )
+}
+//
+
+
 function App() {
+
+    const arr = ['A', 'B', 'C', 'D'];
+    console.log(arr);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+            
+              <Hello arr={arr} />;  //calling a component and passing array
+     </div>
   );
 }
 
