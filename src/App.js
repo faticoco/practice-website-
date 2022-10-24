759ab5df3d8b80943e3b693b3bf463de846e8252
fatima <fb71459@gmail.com>
2022-10-24 21:40:15 +0500

    //adding divs/image on the page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 //component added
-const Hello = ({arr}) => {
+const Hello = ({ arr }) => {
     return (
-        <div>
+        <div >
             {
                 arr.map(title => {
-                    return <div key={title}>{title}</div>    //printing the array using map function
+                    return <div style={{ display: 'inline' }} key={title}>{title}   </div>    //printing the array using map function
                 })
             }
         </div>
@@ -15,16 +16,37 @@ const Hello = ({arr}) => {
 }
 //
 
+//adding divs on the page
+const adding_divs_home_page =()=>
+{
+    <style >
+        .bgimg
+        {
+            <img src="pink.jpg" width="500" height="600"/>
+        }
+    </style>
+
+    return (
+        <div class="bgimg">
+        <h2>This is a heading in a div element</h2>
+        <p>This is some text in a div element.</p>
+    </div>
+        )
+}
+
 
 function App() {
 
-    const arr = ['A', 'B', 'C', 'D'];
+    const arr = ["HOME", "ABOUT", "PORTFOLIO", "PAGES","BLOG","CONTACT"];
     console.log(arr);
-  return (
+    return (
+        //calling a component and passing array
       <div>
             
-              <Hello arr={arr} />;  //calling a component and passing array
+            <Hello arr={arr} />;
+            <adding_divs_home_page />;
      </div>
+
   );
 }
 
